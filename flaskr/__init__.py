3dc3ddf9dae5799f6a95631d05e2371c1aa477a2
Raymong-Zh <yueyue_freedom@163.com>
2018-09-27 16:51:15 +0800

    add some files

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -3,6 +3,7 @@ import os
 from flask import Flask
 
 
+
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
@@ -28,5 +29,14 @@ def create_app(test_config=None):
     @app.route('/hello')
     def hello():
         return 'Hello, World!'
+    
+    from . import db
+    db.init_app(app)
+    
+    from . import auth
+    app.register_blueprint(auth.bp)
+    from . import blog
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
 
     return app
